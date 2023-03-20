cf4bdd38d1da19690dd9012f645233a5c7e9af0e
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-03-20 17:04:06 +0500

    Create item.rb
    In this commit
    - Add item class
    - Add all properties for the class using the UML diagram.

---
+++ Classes/item.rb
@@ -0,0 +1,31 @@
+class Item
+  def initialize(publish_date)
+    @id = rand(1..1000)
+    @genre = genre
+    @author = author
+    @source = source
+    @lable = lable
+    @publish_date = publish_date
+    @archived = false
+  end
+
+  def genre(new_genre)
+    @genre = new_genre
+    new_genre.items.push(self) unless new_genre.items.include?(self)
+  end
+
+  def author(new_author)
+    @author = new_author
+    new_author.items.push(self) unless new_author.items.include?(self)
+  end
+
+  def source
+    @source = new_source
+    new_source.items.push(self) unless new_source.items.include?(self)
+  end
+
+  def lable
+    @lable = new_lable
+    new_lable.items.push(self) unless new_lable.items.include?(self)
+  end
+end
