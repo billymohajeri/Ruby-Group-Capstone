3f5dfb209405ae449ca7bb858bee9b1041aa14c2
Billy Mohajeri <billy.mohajeri@gmail.com>
2023-03-21 09:35:47 +0200

    Improve menu of options

--- main.rb
+++ main.rb
@@ -1,22 +1,15 @@
 require 'io/console'
-#require './show_menu'
-#require './process_options'
-require_relative './show_menu'
-require_relative './process_options'
+require './show_menu'
+require './process_options'
 
 
 def main
-  #$stdout.clear_screen
-  system("cls") || system("clear")
-  # this line added newly
-  puts "\e[32mWelcome to Catalog of my things App!\e[0m"
-  puts
+  $stdout.clear_screen
   loop do
     show_menu
     user_input = gets.chomp.upcase
     puts
     if user_input == 'D'
-      #puts "\e[31mThank you for using this app! ♥️"
       puts "\e[31mThank you for using this app! ♥️ \e[0m"
       exit
     end
