7625f2f3d0df2336f6d3c00d61b98bed85b1b0a5
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-03-21 15:57:46 +0500

    Update book.rb
    Made required change.

--- Classes/book.rb
+++ Classes/book.rb
@@ -3,7 +3,7 @@ require './item'
 class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize(publisher, cover_state)
+  def initialize(publisher, cover_state, publish_date)
     super(publish_date)
     @publisher = publisher
     @cover_state = cover_state
