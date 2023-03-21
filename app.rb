473203bf893d7daa68a8b0561daae60f35ac5037
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-03-21 19:50:47 +0500

    Update app.rb to include options
    In this commit
    - Add methods for different options for the UI.
    The methods include:
    - List all books
    - List all labels
    - Add a new book.

--- app.rb
+++ app.rb
@@ -1,5 +1,44 @@
+require './Classes/book'
+require './Classes/label'
+
 class App
+  attr_reader :books, :labels
+
   def initialize
-    puts 'constructor'
+    @books = []
+    @labels = []
+  end
+
+  def list_all_books
+    puts '\nNo books added yet' if @books.empty?
+    puts '\nAll Books:\n\n'
+    puts "\nPublisher \t| Cover State \t| Publish Date"
+    puts '\n--------------------------------------------'
+    @books.each do |book|
+      puts "#{book.publisher} \t| #{book.cover_state} \t| #{book.publish_date}"
+      puts '\n--------------------------------------------'
+    end
+  end
+
+  def list_all_labels
+    puts '\nNo labels added yet' if @labels.empty?
+    puts '\nAll Labels:\n\n'
+    puts "\nLabels \t| Color"
+    puts '\n--------------------------------------------'
+    @labels.each do |label|
+      puts "#{label.title} \t| #{label.color}"
+      puts '\n--------------------------------------------'
+    end
+  end
+
+  def add_book
+    puts '\nAdd a new book'
+    puts '\nPublisher:'
+    publisher = gets.chomp
+    puts '\nCover State:'
+    cover_state = gets.chomp
+    puts '\nPublish Date[YYYY/MM/DD]:'
+    publish_date = gets.chomp
+    @books << Book.new(publisher, cover_state, publish_date)
   end
 end
