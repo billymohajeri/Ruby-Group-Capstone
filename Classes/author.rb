e8674f2d0400548f61a76c29a466185fdbb568ed
developertariq <dev.tariqul.islam@gmail.com>
2023-03-21 17:25:45 +0600

    Update author.rb

--- Classes/author.rb
+++ Classes/author.rb
@@ -1,8 +1,10 @@
 class Author
-  attr_reader :name, :items
+  attr_reader :id, :first_name, :last_name, :items
 
-  def initialize(name)
-    @name = name
+  def initialize(first_name, last_name)
+    id = rand(1..1000)
+    @first_name = first_name
+    @last_name = last_name
     @items = []
   end
 
