7550d9f655476374dad83c9068053a0a949f62ed
developertariq <dev.tariqul.islam@gmail.com>
2023-03-21 15:39:26 +0600

    Create author.rb

---
+++ Classes/author.rb
@@ -0,0 +1,13 @@
+class Author
+  attr_reader :name, :items
+
+  def initialize(name)
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.author = self
+  end
+end
