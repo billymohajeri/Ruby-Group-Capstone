d5f760ab9e67455b415eb6f3bc9fdb62d3b69205
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-03-22 22:06:18 +0500

    Update schema.sql
    In this commit
    - Use same names of the tables as the UML diagram.

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,56 +1,65 @@
 CREATE DATABASE catalog_app;
 
-CREATE TABLE authors (
-  id SERIAL PRIMARY KEY,
+CREATE TABLE Author (
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   first_name VARCHAR(255),
   last_name VARCHAR(255)
 );
 
-CREATE TABLE genres (
-  id SERIAL PRIMARY KEY,
+CREATE TABLE Genre (
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   name VARCHAR(255)
 );
 
-CREATE TABLE labels (
-  id SERIAL PRIMARY KEY,
+CREATE TABLE Label (
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   title VARCHAR(255),
   color VARCHAR(255)
 );
 
-CREATE TABLE items (
-  id SERIAL PRIMARY KEY,
-  author_id INTEGER REFERENCES authors(id),
-  genre_id INTEGER REFERENCES genres(id),
-  label_id INTEGER REFERENCES labels(id)
+CREATE TABLE Item (
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  author_id INT authors(id),
+  genre_id INT genres(id),
+  label_id INT labels(id)
   publish_date DATE,
   archived BOOLEAN
+
+  FOREIGN KEY ( author_id ) REFERENCES authors(id),
+  FOREIGN KEY ( genre_id ) REFERENCES genres(id),
+  FOREIGN KEY ( label_id ) REFERENCES labels(id)
 );
 
-CREATE TABLE games (
-  id SERIAL PRIMARY KEY,
-  item_id INTEGER REFERENCES items(id),
-  author_id INTEGER REFERENCES authors(id),
-  genre_id INTEGER REFERENCES genres(id),
-  label_id INTEGER REFERENCES labels(id)
+CREATE TABLE Game (
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  item_id INT  items(id),
+  author_id INT  authors(id),
+  genre_id INT  genres(id),
+  label_id INT  labels(id)
   multiplayer BOOLEAN,
   last_played_at DATE
+
+  FOREIGN KEY ( item_id ) REFERENCES items(id),
+  FOREIGN KEY ( author_id ) REFERENCES authors(id),
+  FOREIGN KEY ( genre_id ) REFERENCES genres(id),
+  FOREIGN KEY ( label_id ) REFERENCES labels(id)
 );
 
-CREATE TABLE books (
-  id SERIAL PRIMARY KEY,
-  item_id INTEGER REFERENCES items(id),
-  author_id INTEGER REFERENCES authors(id),
-  genre_id INTEGER REFERENCES genres(id),
-  label_id INTEGER REFERENCES labels(id)
+CREATE TABLE Book (
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  item_id INT REFERENCES items(id),
+  author_id INT REFERENCES authors(id),
+  genre_id INT REFERENCES genres(id),
+  label_id INT REFERENCES labels(id)
   publisher TEXT,
   cover_state TEXT
 );
 
-CREATE TABLE music_albums (
-  id SERIAL PRIMARY KEY,
-  item_id INTEGER REFERENCES items(id),
-  author_id INTEGER REFERENCES authors(id),
-  genre_id INTEGER REFERENCES genres(id),
-  label_id INTEGER REFERENCES labels(id)
+CREATE TABLE MusicAlbum (
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  item_id INT REFERENCES items(id),
+  author_id INT REFERENCES authors(id),
+  genre_id INT REFERENCES genres(id),
+  label_id INT REFERENCES labels(id)
   on_spotify BOOLEAN
 );
