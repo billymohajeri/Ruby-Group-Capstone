ae875b8728d58d35c2df85ab3588753729bd7878
developertariq <dev.tariqul.islam@gmail.com>
2023-03-22 20:06:45 +0600

    Add load_authors method

--- modules/author_module.rb
+++ modules/author_module.rb
@@ -30,4 +30,21 @@ module AuthorModule
     authors = @authors.select { |author| author.id == id }
     authors.first
   end
+
+  def load_authors
+    authors = []
+    data = load_from_file('authors')
+
+    data.map do |items|
+      author = Author.new(items['first_name'], items['last_name'])
+      items['items'].map do |item|
+        if item['type'].include?('Game')
+          game = Game.new(item['publish_date'], item['multiplayer'], item['last_played_at'])
+          author.add_item(game)
+        end
+      end
+      authors << author
+    end
+    authors
+  end
 end
