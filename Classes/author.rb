52a68f8a01f4430ddab424f45574142e937dc3cc
developertariq <dev.tariqul.islam@gmail.com>
2023-03-22 20:04:36 +0600

    Add to_hash method

--- Classes/author.rb
+++ Classes/author.rb
@@ -12,4 +12,13 @@ class Author
     @items << item
     item.author = self
   end
+
+  def to_hash
+    {
+      id: @id,
+      first_name: @first_name,
+      last_name: @last_name,
+      items: @items.map(&:to_hash)
+    }
+  end
 end
