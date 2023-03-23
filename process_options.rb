2ecc47b745dc512cf6eeb05d6663c8852916383e
developertariq <dev.tariqul.islam@gmail.com>
2023-03-23 17:47:23 +0600

    Change methods of games and authors

--- process_options.rb
+++ process_options.rb
@@ -2,10 +2,10 @@ def process_options(usr_inp, app)
   options = {
     '1' => -> { app.list_all_books },
     '2' => -> { app.list_music_albums },
-    '3' => -> { app.list_games },
+    '3' => -> { app.show_list_games },
     '4' => -> { app.list_genres },
     '5' => -> { app.list_all_labels },
-    '6' => -> { app.list_authors },
+    '6' => -> { app.show_list_authors },
     '7' => -> { app.add_book },
     '8' => -> { app.add_music_album },
     '9' => -> { app.add_game }
@@ -15,6 +15,6 @@ def process_options(usr_inp, app)
   if options.key?(chosen_option)
     options[chosen_option].call
   else
-    puts "\e[31mInvalid selection! please try again.\e[0m\n"
+    puts "\e[31mInvalid selection! Please try again.\e[0m\n"
   end
 end
