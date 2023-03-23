9a2fbdf0b21949d34e6673cbdbbaed0f7695b0aa
Billy Mohajeri <billy.mohajeri@gmail.com>
2023-03-23 10:25:06 +0200

    Remove extra menu items

--- show_menu.rb
+++ show_menu.rb
@@ -2,16 +2,13 @@ def show_menu
   puts "\nPlease choose an option by entering a number:
     1 - List all books
     2 - List all music albums
-    3 - List all movies
-    4 - List of games
-    5 - List all genres
-    6 - List all labels
-    7 - List all authors
-    8 - List all sources
-    9 - Add a book
-    A - Add a music album
-    B - Add a movie
-    C - Add a game
-    \e[31mD - Exit\e[0m"
+    3 - List of games
+    4 - List all genres
+    5 - List all labels
+    6 - List all authors
+    7 - Add a book
+    8 - Add a music album
+    9 - Add a game
+    \e[31m0 - Exit\e[0m"
   print "\nYour choice: "
 end
