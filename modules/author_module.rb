0a923a3e724ddc5d831a049a6ce800aafa35d0a8
developertariq <dev.tariqul.islam@gmail.com>
2023-03-23 17:44:28 +0600

    Display index number in list

--- modules/author_module.rb
+++ modules/author_module.rb
@@ -1,17 +1,20 @@
 module AuthorModule
-  def list_authors
+  def show_list_authors
     if @authors.empty?
       puts "\n\e[31mNo authors available!\e[0m\n"
-      puts
-    else
+    else  
       puts "\nList of Authors \n\n"
-      puts '---------------------------------------------------------'
-      puts "| ID \t\t| Name \t\t\t\t\t|"
-      puts '---------------------------------------------------------'
-      @authors.each_with_index do |author, _index|
-        puts "| #{author.id} \t\t| #{author.first_name} #{author.last_name} \t\t\t\t|"
-        puts '---------------------------------------------------------'
-      end
+      list_authors
+    end
+  end
+
+  def list_authors
+    puts '-----------------------------------------------------------------------------------'
+    puts "| Index \t| ID \t\t| Name \t\t\t\t\t\t|"
+    puts '-----------------------------------------------------------------------------------'
+    @authors.each_with_index do |author, index|
+      puts "| #{index} \t\t| #{author.id} \t\t| #{author.first_name} #{author.last_name} \t\t\t\t|"
+      puts '-----------------------------------------------------------------------------------'
     end
   end
 
@@ -27,7 +30,7 @@ module AuthorModule
   end
 
   def find_author(id)
-    authors = @authors.select { |author| author.id == id }
+    authors = @authors.select { |author| author.id == id }   
     authors.first
   end
 
