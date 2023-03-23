0759bc08a4294d9db7612882f2fdb0457fe07948
developertariq <dev.tariqul.islam@gmail.com>
2023-03-23 17:44:58 +0600

    Display index number in the list

--- modules/game_module.rb
+++ modules/game_module.rb
@@ -1,18 +1,23 @@
 module GameModule
-  def list_games
+  def show_list_games
     if @games.empty?
       puts "\n\e[31mNo games available!\e[0m\n"
       puts
     else
       puts "\nList of Games\n\n"
-      puts '-------------------------------------------------------------------------'
-      puts "| Publish Date \t\t| Mode \t\t\t| Last Played At \t|"
-      puts '-------------------------------------------------------------------------'
-      @games.each do |game|
-        puts "| #{game.publish_date} \t\t| #{game.multiplayer ? 'Multiplayer' : 'Singleplayer'} \t\t| #{game.last_played_at} \t\t|"
-        puts '-------------------------------------------------------------------------'
-      end
+      list_games
     end
+  end
+  def list_games
+
+    puts '-----------------------------------------------------------------------------------------'
+    puts "| Index \t| Publish Date \t\t| Mode \t\t\t| Last Played At \t|"
+    puts '-----------------------------------------------------------------------------------------'
+    @games.each_with_index do |game, index|
+      puts "| #{index} \t\t| #{game.publish_date} \t\t| #{game.multiplayer ? 'Multiplayer' : 'Singleplayer'} \t\t| #{game.last_played_at} \t\t|"
+      puts '-----------------------------------------------------------------------------------------'
+    end
+    
   end
 
   def add_game()
@@ -31,18 +36,24 @@ module GameModule
   end
 
   def game_author
-    print 'Do you want to add new author (1) or select from the list (2)? [Input the number]: '
+    print "\nDo you want to add author (1) or show list (2)? [Input the number]: "
     type = gets.chomp.to_i
     case type
     when 1
       add_author
-      author_id = @authors[-1].id
+      author_id = @authors.length - 1
     when 2
-      list_authors
-      print "\nEnter author ID: "
-      author_id = gets.chomp.to_i
-    end
-    find_author(author_id)
+      if @authors.empty? 
+        puts "\n\e[31mNo authors available, please add an author!\e[0m\n"
+        add_author
+        author_id = @authors.length - 1
+      else
+        puts "\nSelect an author from the following list by index (not id) \n"
+        list_authors
+        author_id = idx_validate(@authors, gets.chomp.to_i)
+      end
+    end    
+    @authors[author_id]   
   end
 
   def load_games
